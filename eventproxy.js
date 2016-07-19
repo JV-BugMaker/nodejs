be3126a57d92acb012adf5fd18048ae02332eed0
JV-BugMaker <794241063@qq.com>
2016-07-20 01:08:04 +0800

    [*]增加在eventproxy中的代码注释

--- eventproxy.js
+++ eventproxy.js
@@ -1,3 +1,4 @@
+//引入依赖包
 var eventproxy = require('eventproxy');
 var superagent = require('superagent');
 var cheerio = require('cheerio');
@@ -11,6 +12,7 @@ superagent.get(cnodeUrl)
       return console.error(err);
     }
     var topicUrls = [];
+  //解析DOM HTML而已
     var $ = cheerio.load(res.text);
     $('#topic_list .topic_title').each(function (idx, element) {
       var $element = $(element);
@@ -19,7 +21,9 @@ superagent.get(cnodeUrl)
     });
 
     var ep = new eventproxy();
-
+    //监听
+// 命令 ep 重复监听 topicUrls.length 次（在这里也就是 40 次） `topic_html` 事件再行动
+// 就是在遍历topicUrls中 ep。emit触发topic_html事件 这边先初始化后就开始监听此事件 因此会相互呼应
     ep.after('topic_html', topicUrls.length, function (topics) {
       topics = topics.map(function (topicPair) {
         var topicUrl = topicPair[0];
@@ -39,6 +43,7 @@ superagent.get(cnodeUrl)
     topicUrls.forEach(function (topicUrl) {
       superagent.get(topicUrl)
         .end(function (err, res) {
+          //这边不能增加err判断 否则就会出现错误
           console.log('fetch ' + topicUrl + ' successful');
           ep.emit('topic_html', [topicUrl, res.text]);
         });
