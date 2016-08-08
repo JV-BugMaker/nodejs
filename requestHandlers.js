7338e26da6682bbb8254fa35d292dda48e244736
JV-BugMaker <794241063@qq.com>
2016-08-08 23:22:57 +0800

    [*]增加http handle处理

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,13 @@
 function start()
 {
     console.log("request start handle there");
+    return "Hello Start";
 }
 
 function upload()
 {
     console.log("request upload handle there");
+    return "Hello Upload";
 }
 function favicon()
 {
