e7c41cdba6146dd735a55c10ae2a6e6ca6139ccb
JV-BugMaker <794241063@qq.com>
2017-04-18 23:40:37 +0800

    [*]注释

--- emitter.js
+++ emitter.js
@@ -38,3 +38,14 @@ req.write('data\n');
 req.end();
 
 //事件订阅发布方式使得我们只需要各个事件的节点即可
+
+//如果对一个事件的监听超过10个 会引起警告 是node基于单线程运行 过多的监听器会导致内存泄漏 
+//emitter.setMaxListeners(0) 可以将这个限制去掉 由于事件发布会引擎侦听器执行，会导致CPU占用过高的场景。
+//一个健壮的EventEmitter一定包括对error的监听，否则会导致进程的退出
+
+function Stream(){
+    events.EventEmitter.call(this);
+}
+
+util.inherits(Stream,events.EventEmitter);
+
