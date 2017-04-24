d29f193176d45cd645b32fac609a2d6e9dd0bdcf
JV-BugMaker <794241063@qq.com>
2017-04-25 00:33:16 +0800

    [*]obj

--- promise.js
+++ promise.js
@@ -39,3 +39,32 @@ Deferred.prototype.success = function(data){
     this.state = 'success';
     this.promise.emit('progress',data);
 }
+
+
+var promisify = function(res){
+    var deferred = new Deferred();
+
+    var result = '';
+    res.on('data',function(chunk){
+        result += chunk;
+        deferred.progress(chunk);
+    });
+
+    res.on('end',function(){
+        deferred.resolve(result);
+    });
+    res.on('error',function(err){
+        deferred.reject(err);
+    });
+
+    return deferred.promise;
+}
+
+promisify.then(function(){
+    //done
+},function(err){
+
+},function(chunk){
+    console.log(chunk);
+});
+
