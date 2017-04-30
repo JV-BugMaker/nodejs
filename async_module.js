63de9a50df7958a87cc7c4c8d9f68a9ed3d69fab
JV-BugMaker <794241063@qq.com>
2017-04-30 19:51:03 +0800

    [*]实现async并行调用parallel

--- async_module.js
+++ async_module.js
@@ -14,3 +14,18 @@ async.series([
 //series中传入的函数callback并非由使用者指定 ，此处的回调函数是由async通过高阶函数的方式注入
 //每个callback执行时会将结果保存起来,然后执行下一个调用，直到结束所有调用.
 //队列里的异步调用保存的结果以数组的方式传入，这里的异常处理，一旦出现异常就结束所有调用。
+
+//异步并行调用
+async.parallel([
+    function(callback){
+        fs.readFile('file1.txt','utf-8',callback);
+    },
+    function(callback){
+        fs.readFile('file2.txt','utf-8',callback);
+    }
+],function(err,result){
+    //result=>[file1.txt,file2.txt]
+});
+
+//异步并行调用 注入的回调函数 比较重要
+
