450652b8a7d346c8836a758cfaf2ed0a0d99616a
JV-BugMaker <794241063@qq.com>
2017-06-05 22:37:34 +0800

    [*]并发执行step

--- step.js
+++ step.js
@@ -1,7 +1,7 @@
 //step(task1,task2,task3) step接收任意数量的任务 所有任务都将会串行一次执行
 
 var fs = require('fs');
-step(
+Step(
     function readFile1(){
         fs.readFile('file1.txt','utf-8',this);
     },
@@ -16,3 +16,14 @@ step(
 //step与前面提到的promise、事件模型有一个不同点就是用到了this 
 //this在这边的作用就是step中的next()方法，将异步调用的结果传递给下一个任务作为参数，并调用执行。
 
+//并行任务执行 用到了this中的parallel()方法
+
+Step(
+    function readFile1(){
+        fs.readFile('file1.txt','utf-8',this.parallel());
+        fs.readFile('file2.txt','utf-8',this.parallel());
+    },
+    function done(err,content1,content2){
+        console.log(arguments);
+    }
+);
