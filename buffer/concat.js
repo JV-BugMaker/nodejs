50b657dfbbf32725830f987e552e9f4c891fd76a
JV-BugMaker <794241063@qq.com>
2017-06-21 00:15:02 +0800

    [*]buffer拼接

---
+++ buffer/concat.js
@@ -0,0 +1,30 @@
+
+Buffer.concat = function(list,length){
+    if(!Array.isArray(list)){
+        throw new Error('Usage:Buffer.concat(list,[length])');
+    }
+
+    if(list.length === 0 ){
+        return new Buffer(0);
+    }else if(list.length === 1){
+        return list[0];
+    }
+
+    if(typeof length !== 'number'){
+        length = 0;
+        for(var i =0;i < length;i++){
+            var buf = list[i];
+            length += buf.length;
+        }
+    }
+
+    var buffer = new Buffer(length);
+    var pos = 0;
+    for(var i = 0;i < list.length;i++){
+        var buf = list[i];
+        buf.copy(buff,pos);
+        pos += buf.length;
+    }
+
+    return buffer;
+}
--- buffer/transfer.js
+++ buffer/transfer.js
@@ -4,4 +4,13 @@ buf.write(str,[offset],[length],[encoding]);
 
 //buffer 转换成字符串
 
-buf.toString();
+buf.toString([encoding],[start],[end]);
+
+//可以指定编码进行转换 指定位置
+
+Buffer.isEncoding(encoding); //来判断是否支持转换编码
+
+setEncoding('utf-8');  //能够解决大部分的宽字节乱码问题 但是没有从根本上解决
+string_decoder(); //从根本上来解决  但是编码有限制 
+
+//解决方式 其实是 识别出编码的字节数 然后按照字节数的整数倍进行输出 不在拆分成2部分
