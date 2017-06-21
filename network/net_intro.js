fa51c4dbf52f1c69f841325423e6fbbbcf6a4435
JV-BugMaker <794241063@qq.com>
2017-06-21 22:33:25 +0800

    [*]tcp服务器

---
+++ network/net_intro.js
@@ -0,0 +1,23 @@
+//net 对应TCP
+//dgram 对应UDP
+//http 对应http https对应https 
+
+var net = require('net');
+
+//基于事件订阅模式
+var server = net.createServer(function(socket){
+    //新的连接
+    socket.on('data',function(data){
+        socket.write("你好");
+    });
+
+    socket.on('end',function(){
+        console.log('连接结束');
+    });
+
+    socket.write('展示');
+});
+
+server.listen(8124,function(){
+    console.log('servr bound');
+});
