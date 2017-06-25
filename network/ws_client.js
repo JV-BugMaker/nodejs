a190d84ba3d8614c6bdf4fd5b9bd0a86b28da341
JV-BugMaker <794241063@qq.com>
2017-06-25 21:01:51 +0800

    [*]完整模拟浏览器协议升级

--- network/ws_client.js
+++ network/ws_client.js
@@ -13,3 +13,47 @@ socket.onmessage = function(event){
 
 var crypto = require('crypto');
 var val = crypto.createHash('sha1').update(key).digest('bash64');
+
+
+var WebSocket = function(url){
+    this.options = parseUrl(url);
+    this.connect();
+};
+
+WebSocket.prototype.onopen = function(){
+    //TODO
+}
+
+WebSocket.prototype.setSocket = function(socket){
+    this.socket = socket;
+}
+
+WebSocket.prototype.connect = function(){
+    var that = this;
+    var key = new Buffer(this.options.protocolVersion + '-' + Date.now()).toString('base64');
+    var shasum = crypto.createHash('sha1');
+    var expected = shasum.update(key + '258EAFA5-E914-47DA-95CA-C5ABoD85B11').digest('base64');
+
+    var options = {
+        port:this.options.port,
+        host:this.options.host,
+        header:{
+            'Connection':'Upgrade',
+            'Upgrade':'websocket',
+            'Sec-WebSocket-Version':this.options.protocolVersion,
+            'Sec-WebSocket-Key':key
+        }
+    };
+
+    var req = http.request(options);
+    req.send();
+
+    req.on('Upgrade',function(res,socket,upgradeHead){
+        //连接成功
+        that.setSocket(socket);
+        //open
+        that.onopen();
+    });
+}
+
+
