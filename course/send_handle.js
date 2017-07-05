4248d079cb7d0e3cb58163bf699e5c3d68dc440b
JV-BugMaker <794241063@qq.com>
2017-07-05 23:43:47 +0800

    [*]进程之间传递句柄 实际上是对消息的封装

--- course/send_handle.js
+++ course/send_handle.js
@@ -20,3 +20,34 @@ process.on('message',function(m,sever){
         });
     }
 });
+
+
+//===============================http层面============================
+
+var cp = require('child_process');
+var child1 = cp.fork('child.js');
+var child2 = cp.fork('child.js');
+
+var server = require('http').createServer();
+server.listen(1337,function(){
+    child1.send('server',server);
+    child2.send('server',server);
+    //turnoff server
+    server.close();
+});
+
+
+//child.js
+var http = require('http');
+var server = http.createServer(function(req,res){
+    res.writeHeader(200,{'Content-type':'text/plain'});
+    res.end('handle by child,pid is' + process.pid + '\n');
+});
+
+process.on('message',function(m,tcp){
+    if(m === 'server'){
+        tcp.on('connection',function(socket){
+            server.emit('connection',socket);
+        });
+    }
+});
