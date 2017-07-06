c2a4841371a5a874168b8093cf45c019470683ca
JV-BugMaker <794241063@qq.com>
2017-07-06 22:56:51 +0800

    [+]信号事件

---
+++ course/signal.js
@@ -0,0 +1,10 @@
+
+
+process.on('SIGTERM',function(){
+    console.log('Got a SIGTERM,exiting');
+    process.exit(1);
+});
+
+console.log('server running with PID',process.pid);
+process.kill(process.pid,'SIGTERM');
+
